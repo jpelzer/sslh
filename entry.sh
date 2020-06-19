2ead88bfdd994fad4cf165415b5bae66b4147725
Jason Pelzer <jason@pelzer.com>
2020-06-19 16:09:17 -0400

    Re-added transparent

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sslh --foreground --numeric -u root \
+sslh --foreground --transparent --numeric -u root \
    --listen $LISTEN_IP:$LISTEN_PORT \
    --tls $HTTPS_HOST:$HTTPS_PORT \
    --anyprot $SSH_HOST:$SSH_PORT \
