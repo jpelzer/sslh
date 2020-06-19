eb6b21946c6408f6964d61f93661981fcc3571c2
Jason Pelzer <jason@pelzer.com>
2020-06-19 15:44:44 -0400

    Switched order of protocols to put tls first

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sslh --transparent -f -u root --listen $LISTEN_IP:$LISTEN_PORT \
-   --ssh $SSH_HOST:$SSH_PORT \
    --tls $HTTPS_HOST:$HTTPS_PORT \
+   --ssh $SSH_HOST:$SSH_PORT \
    $@
